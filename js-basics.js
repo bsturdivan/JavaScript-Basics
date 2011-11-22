623d65f67d941a655f99db0f1dce20763fb5191b
unknown <brian.sturdivan@SDBSturdivanLT1.qnao.net>
2011-11-22 11:48:16 -0800

    initial upload

--- js-basics.js
+++ js-basics.js
@@ -1,27 +1,27 @@
 if (!("a" in window)) {
     var a = 1;
 }
-alert(a);
+console.log(a);
 
 var a = 1,
     b = function a(x) {
         x && a(--x);
     };
-alert(a);
+console.log(a);
 
 function a(x) {
     return x * 2;
 }
 var a;
-alert(a);
+console.log(a);
 
 function b(x, y, a) {
     arguments[2] = 10;
-    alert(a);
+    console.log(a);
 }
 b(1, 2, 3);
 
 function a() {
-    alert(this);
+    console.log(this);
 }
 a.call(null);
